1b22b60eff8307a50fbfe3e057d24fbce9a97fc9
Philipp Buchinger <buchinger@proton.me>
2022-08-09 13:12:16 +0200

    added comments and rearranged items

--- nvim/lua/buphio/set.lua
+++ nvim/lua/buphio/set.lua
@@ -1,26 +1,29 @@
--- vim.opt.guicursor = ""
-
+-- color
 vim.opt.termguicolors = true
 
+-- lines
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.colorcolumn = "100"
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+
+-- modes
 vim.opt.cmdheight = 2
 vim.opt.showmode = false
-vim.opt.listchars:append({trail = '⋅', lead = '⋅'})
-vim.opt.list = true
 vim.opt.mouse = "a"
 
+-- invisibles
+vim.opt.listchars:append({trail = '⋅', lead = '⋅'})
+vim.opt.list = true
+
+-- tabstops / spacing
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
-
 vim.opt.smartindent = true
-
-vim.opt.wrap = false
