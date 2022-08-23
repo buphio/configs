e3cb740c957d574eee9acf6dab901766e6caa86e
Philipp Buchinger <buchinger@proton.me>
2022-08-23 10:42:58 +0200

    deleted lsp settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,7 +4,5 @@ require("user.keymaps")
 -- plugins
 require("user.plugins")
 require("user.treesitter")
-require("user.lsp")
 require("user.color")
 require("user.lualine")
---require("user.bufferline")
